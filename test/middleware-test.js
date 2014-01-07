12e45662fe4cf4bfc2d3403adcb18a1a956c6945
fent <933490+fent@users.noreply.github.com>
2014-01-07 17:36:17 -0500

    middleware.unknownCommand tests

--- test/middleware-test.js
+++ test/middleware-test.js
@@ -1,2 +1,36 @@
 var middleware = require('../lib/middleware');
+var errors     = require('../lib/replies/en').errors;
 var assert     = require('assert');
+
+
+describe('unknownCommand', function() {
+  var commands = ['a', 'foo'];
+
+  describe('without `inc`', function() {
+    var fn = middleware.unknownCommand(commands);
+
+    it('gives error when calling unknown command', function() {
+      fn(function(err, cmd) {
+        assert.equal(err, errors.unknownCommand);
+        assert.equal(cmd, 'ohoh');
+      }, null, 'ohoh');
+    });
+
+    it('gives no error with the right command', function() {
+      fn(function(err) {
+        assert.ok(!err);
+      }, null, 'foo');
+    });
+  });
+
+  describe('with `inc` defined', function() {
+    var fn = middleware.unknownCommand(commands, 'set');
+
+    it('gives custom error when calling unknown command', function() {
+      fn(function(err, cmd) {
+        assert.equal(err, errors.unknownCommand);
+        assert.equal(cmd, 'set ohoh');
+      }, null, 'ohoh');
+    });
+  });
+});
